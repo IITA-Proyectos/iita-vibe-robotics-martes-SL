9b0502011e492340961ffa577153b8206a9b6284
sagripantimaximo-oss <sagripantimaximo@gmail.com>
2026-05-25 13:32:24 -0300

    Add files via upload

---
+++ alumnos/maximo/practicas_python/mitad.tarea5.py
@@ -0,0 +1,23 @@
+materiales = ["motores", "ruedas", "sensores", "bateria", "cables"]
+
+print(materiales[2])
+
+acciones = []
+
+acciones.append("avansar 30 cm")
+acciones.append("girar 90 grados")
+acciones.append("avansar 30 cm")
+acciones.append("girar 90 grados")
+acciones.append("avansar 30 cm")
+acciones.append("girar 90 grados")
+acciones.append("avansar 30 cm")
+acciones.append("girar 90 grados")
+acciones.append("avansar 30 cm")
+acciones.append("girar 90 grados")
+
+print(acciones)
+
+acciones.pop()
+acciones.pop()
+
+print(acciones)

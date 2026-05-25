4846635beaef05468ff574b46498998a5779a455
sagripantimaximo-oss <sagripantimaximo@gmail.com>
2026-05-25 13:25:14 -0300

    Add files via upload

---
+++ alumnos/maximo/practicas_python/tarea1.py
@@ -0,0 +1,35 @@
+NombreMaximo = ( "Maximo" )
+Separacion_ruedas = ( 560 )
+cantidad_motores = ( 2 )
+robot_estado_encendido = ("falso")
+
+print (type(NombreMaximo))
+print (type(Separacion_ruedas))
+print (type(cantidad_motores))
+print (type(robot_estado_encendido))
+
+a = ( 16 )
+b = ( 32 )
+
+resta_a_menos_b = ( 16 + 32 )
+suma_a_mas_b = ( 32 - 16)
+mult_a_mult_b = ( 16 * 32 )
+divicion_a_dividido_b = ( 32 // 16 )
+modulo_a_b = ( 16 % 32 )
+
+print (type(resta_a_menos_b))
+print (type(suma_a_mas_b))
+print (type(mult_a_mult_b))
+print (type(divicion_a_dividido_b))
+print (type(modulo_a_b))
+
+print (type( "73" ))
+print (type( False ))
+print (type( 13.1415 ))
+print (type (99 ))
+print (type( True + 1 ))
+
+nombre = input( "¿Cómo te llamás? " )
+edad = int(input( "¿Cuántos años tenés? " ))
+edad_futura = edad + 10
+print ( "Hola (nombre), dentro de 10 años vas a tener (edad_futura) años" )

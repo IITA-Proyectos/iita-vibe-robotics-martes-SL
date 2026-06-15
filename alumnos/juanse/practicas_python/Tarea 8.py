2cfb00608e2c4a48d079dda7b75df386a73f173a
Juanse-bike <marijuansegundo@gmail.com>
2026-06-15 18:46:35 -0300

    Subo tarea 8 del 15/06

---
+++ alumnos/juanse/practicas_python/Tarea 8.py
@@ -0,0 +1,54 @@
+# Crea una función llamada mostrar_bienvenida() que imprima por pantalla un cartel decorativo bien bonito para el inicio de nuestro programa.
+# Invocala en el lugar y momento que corresponda.
+
+
+def mostrar_bienvenida():
+    print("====================================")
+    print("¡BIENVENIDOS AL FIXTURE DEL MUNDIAL!")
+    print("====================================")
+
+
+mostrar_bienvenida()
+
+# En nuestro anterior programa seguramente repetimos muchas veces un print
+# con un separador con guiones, pero quizá algunos no tenían la misma cantidad
+# de guiones o a veces nos olvidábamos de imprimirlo. Debes crear una función
+# para imprimir un bonito separador. Luego, invocala donde corresponda.
+
+
+def separador():
+    print("---------------***---------------")
+
+
+separador()
+
+# Crea una función llamada festejar_campeón() que muestre un festejo con
+# emojis y estrellitas cuando se ingresa el campeón final del fixture:
+
+
+def festejar_campeón():
+    print("🏆🏆🏆🏆🏆🏆🏆🏆🏆🏆🏆🏆🏆🏆🏆🏆🏆🏆")
+    print("🎉 ¡ATENCIÓN! ¡TENEMOS UN NUEVO CAMPEÓN! 🎉")
+    print("⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐")
+
+festejar_campeón()
+
+# Escribí una función llamada mostrar_menu() que le presente al usuario algunas 
+#opciones (por ahora ficticias) disponibles en nuestro programa:
+
+def mostrar_menu():
+    print("MENÚ DEL FIXTURE")
+    print("1. Ver grupos")
+    print("2. Definir octavos")
+    print("3. Definir cuartos")
+    print("4. Salir")
+
+mostrar_menu()
+
+# 5
+
+def reportar_motores():
+    print("Motores inicializados correctamente")
+    print("Estado: Activos y listos")
+
+reportar_motores()
